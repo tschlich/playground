23b548bffb51c04375fc113a8aa199e49e5e188f
tschlich <t.schlich@mad-world.de>
2016-06-19 12:11:10 +0200

    check www folder with absolute path

--- scripts/setVhosts.sh
+++ scripts/setVhosts.sh
@@ -4,10 +4,10 @@
 # Do not run this outside the vaprobash vm!
 
 # back up and delete folder "www"
-if [ -d "www" ]; then
+if [ -d "/vagrant/www" ]; then
   tar -czf "backup_www_$(date '+%Y.%m.%d_%H-%M-%S').tar.gz" /vagrant/www
 fi
 
-vhost -d /vagrant/www/frontend/htdocs -s frontend.192.168.19.71.xip.io -a frontend.test1.192.168.19.71.xip.io
-vhost -d /vagrant/www/backend/htdocs -s backend.192.168.19.71.xip.io -a backend.192.168.19.71.xip.io
+vhost -d /vagrant/www/frontend/htdocs -s frontend.192.168.19.83.xip.io -a frontend.test1.192.168.19.83.xip.io
+vhost -d /vagrant/www/backend/htdocs -s backend.192.168.19.83.xip.io -a backend.192.168.19.83.xip.io
 
