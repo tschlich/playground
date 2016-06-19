5c3460cc530b818ae718f68ccded40457c410098
tschlich <t.schlich@mad-world.de>
2016-06-19 12:36:23 +0200

    fixed ip

--- scripts/setVhosts.sh
+++ scripts/setVhosts.sh
@@ -8,6 +8,6 @@ if [ -d "/vagrant/www" ]; then
   tar -czf "backup_www_$(date '+%Y.%m.%d_%H-%M-%S').tar.gz" /vagrant/www
 fi
 
-vhost -d /vagrant/www/frontend/htdocs -s frontend.192.168.19.83.xip.io -a frontend.test1.192.168.19.83.xip.io
-vhost -d /vagrant/www/backend/htdocs -s backend.192.168.19.83.xip.io -a backend.192.168.19.83.xip.io
+vhost -d /vagrant/www/frontend/htdocs -s frontend.192.168.19.71.xip.io -a frontend.test1.192.168.19.71.xip.io
+vhost -d /vagrant/www/backend/htdocs -s backend.192.168.19.71.xip.io -a backend.192.168.19.71.xip.io
 
