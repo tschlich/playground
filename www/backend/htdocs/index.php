aa569005f6e06ed15c9b412b3a85dbebe47e6d0d
Thomas Schlich <t.schlich@mad-world.de>
2016-06-19 16:59:28 +0000

    frontend to backend switching and same theme for all 3 webs

---
+++ www/backend/htdocs/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Playground - Backend</title>
+    <link rel="stylesheet" href="/assets/css/mad.dev.css">
+  </head>
+
+  <body>
+    <div class="container" id="wrapper">
+      <header>
+        <h2><a href="http://192.168.19.83.xip.io">Vaprobash Playground</a></h2>
+        <h3><a href="http://backend.192.168.19.83.xip.io">Backend</a></h3>
+		<p><a href="http://frontend.192.168.19.83.xip.io">go to frontend</a></p>
+	  </header>
+
+      <article>
+		<h1>Nothing here yet!</h1>
+		<p>what about software pattern examples like - of course - MVC?</p>
+		<p>what about templating?</p>
+		<h3>be creative!</h3>
+      </article>
+      <footer><a href="http://www.mad-world.de">2016 mad-world.de</a></footer>
+    </div><!-- /#wrapper -->
+  </body>
+</html>
--- www/frontend/htdocs/assets/js/app.js
+++ www/frontend/htdocs/assets/js/app.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   $('a').each(function() {
     var self = new RegExp('/' + window.location.host + '/');
-    if (!self.test(this.href)) {
+    if (!self.test(this.href) && $(this).hasClass('internal') !== true ) {
       $(this).attr("target","_blank");
     }
   });
