c2efca562155c826876e70952ce3e43bf9844a62
tschlich <t.schlich@mad-world.de>
2016-06-19 13:54:33 +0200

    node modules and bower packages - grunt tasks

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,9 @@ if [ -d "/vagrant/www" ]; then
   tar -czf "backup_www_$(date '+%Y.%m.%d_%H-%M-%S').tar.gz" /vagrant/www
 fi
 
+# create vhosts
 vhost -d /vagrant/www/frontend/htdocs -s frontend.192.168.19.71.xip.io -a frontend.test1.192.168.19.71.xip.io
 vhost -d /vagrant/www/backend/htdocs -s backend.192.168.19.71.xip.io -a backend.192.168.19.71.xip.io
 
+# install node modules and bower packages, run grunt tasks "build" and "watch"
+su - vagrant -c 'cd /vagrant/www/frontend; npm install; bower install; grunt build; grunt watch &;'
